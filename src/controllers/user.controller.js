a5b02ef6c9db4abb4f9baa0ab305e57e2c26d7ec
yeasinshakil <shakilrock1998@gmail.com>
2024-03-02 23:09:29 +0600

    developing register form

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,22 @@
+import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'ok shakil bhai'
-    })
+    // get user information from frontend
+    // validaton - not empty
+    // check if user already exist- username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object- create entry in DB
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+
+    const { username, fullName, email, password, avata, coverImage } = req.body;
+
+    if ([username, fullName, email, password].some((field) => field.trim() === '')) {
+        throw new ApiError(400, 'All fields required');
+    }
 })
 
 export { registerUser };
